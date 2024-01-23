0b12fe14b493322ae8af684c030bc757bb8e213b
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-23 20:08:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            Console.WriteLine("This is the while loop");
+            while (index <= 5)
+
+                Console.WriteLine(index);
+            index++;
+            {
+                Console.WriteLine("This is the do while loop");
+                int index2 = 0;
+                do
+                {
+                    Console.WriteLine(index2);
+                    index++;
+                }
+                while (index2 <= 5);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
